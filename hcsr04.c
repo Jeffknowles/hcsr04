ab4dbec5217733b3cca07b5a1de6e1bbd9681f41
jknowles <jeff.knowles@gmail.com>
2017-03-06 22:36:20 -0800

    autocommit

--- hcsr04.c
+++ hcsr04.c
@@ -149,7 +149,7 @@ int main(void) {
         gettimeofday(&new_time, NULL);
 
 		// dt = (float) GetTimeStamp(NULL) - last_time;
-		dt = (new_time.tv_sec - last_time.tv_sec);      // sec to ms
+		dt = (double) (new_time.tv_sec - last_time.tv_sec);      // sec to ms
         dt += (new_time.tv_usec - last_time.tv_usec)/1000000;   // us to ms
 		last_time = new_time;
 
