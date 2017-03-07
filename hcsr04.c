c501fcb4d547e4420b7b936bfc0333b7f1e15ccd
jknowles <jeff.knowles@gmail.com>
2017-03-06 22:04:11 -0800

    stuff

--- hcsr04.c
+++ hcsr04.c
@@ -168,7 +168,7 @@ int main(void) {
 		        // }
 		      }
 		      else {
-		        v[ch] = v[ch] - (float) dt / 1000; // otherwise decrment v by dt to record time
+		        v[ch] = v[ch] - (float) dt * 1000; // otherwise decrment v by dt to record time
 		      }
 		    }
 		  }
