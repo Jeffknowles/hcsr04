62197496c56edf62367f022b59bee2ff7c5df2a4
jknowles <jeff.knowles@gmail.com>
2017-03-09 00:25:51 -0800

    autocommit

--- hcsr04.c
+++ hcsr04.c
@@ -131,6 +131,7 @@ int main(void) {
   	ledscape_t *const leds = ledscape_init(num_pixels);
   	uint8_t rgb_spike[3] = {255,255,255};
   	uint8_t rgb_off[3] = {0,0,0};
+  	frame_num = 0; 
   	ledscape_frame_t *const frame = ledscape_frame(leds, frame_num);
   	for (ii=0; ii<num_pixels; ii++){
   		ledscape_set_color(frame, 0, ii, rgb_off[0], rgb_off[1], rgb_off[2]);
