e71b07dc89d458f14e03ee7329d6af15bf0bd592
jknowles <jeff.knowles@gmail.com>
2017-03-09 00:52:44 -0800

    autocommit

--- hcsr04.c
+++ hcsr04.c
@@ -225,7 +225,7 @@ int main(void) {
 		    else { // otherwise if neuron is in spike mode
 		      if (v[ch] < -1 * spike_len[ch]) { // if the time since spike onset is up, end spike
 		        v[ch] = 0; // set voltage to 0
-		        ledscape_set_color(frame, 0, p, rgb_off[0], rgb_off[1], rgb_off[2]);
+		        ledscape_set_color(frame, 0, ch, rgb_off[0], rgb_off[1], rgb_off[2]);
 		        // if (ledPins[ch] > 0) {
 		        //   digitalWrite(ledPins[ch], LOW);
 		        // }
