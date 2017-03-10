2a71c4be5df2bc2361db132c268112945a3bda96
jknowles <jeff.knowles@gmail.com>
2017-03-09 18:07:10 -0800

    autocommit

--- hcsr04.c
+++ hcsr04.c
@@ -114,6 +114,9 @@ int main(void) {
 	// initialize neurons 
 	double v[nch] =  {0};
 	double spike_len[nch] =   {20};
+	for (ii=1; ii<nch; ii++){
+		spike_len[nch] = (double) random_float(10,50);
+	}
 	int connections[nch][maxCon];
 	float weights[nch][maxCon];
 	// generate connections among neurons
