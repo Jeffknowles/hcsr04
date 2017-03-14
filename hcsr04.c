9ccac19410d8ffb903dfe0a90e286b12d984bb74
jknowles <jeff.knowles@gmail.com>
2017-03-14 00:29:41 -0700

    autocommit

--- hcsr04.c
+++ hcsr04.c
@@ -41,7 +41,7 @@ const uint8_t dim_curve[] = {
 
 
 
-const double minIPI = 0.05; // minimum interping interval in miliseconds
+const double minIPI = 0.5; // minimum interping interval in miliseconds
 const double sense_thresh_i = 60; // threshold where responses turn on
 // const int pingPin = 11; // trigger for sonar pulses
 // const int echoPin = 12; // return for sonar pulses
