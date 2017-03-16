d8ad5a5bf803258c2b0423b6c90480661c0e874a
jknowles <jeff.knowles@gmail.com>
2017-03-15 22:06:02 -0700

    autocommit

--- hcsr04.c
+++ hcsr04.c
@@ -133,7 +133,7 @@ int main(void) {
 		weights[ii][3] = 2;
 		for (iii=3; iii<maxCon; iii++){
 			connections[ii][iii]=rand() % nch;
-			weights[ii][iii] = random_float((float) -5, (float) 6);
+			weights[ii][iii] = random_float((float) -4, (float) 6);
 		}
 
 		if (ii > 2){
