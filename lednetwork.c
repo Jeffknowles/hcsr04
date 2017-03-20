416e3c56875589c547058f8f53227f3968b40f21
jknowles <jeff.knowles@gmail.com>
2017-03-20 11:29:25 -0700

    autocommit

--- lednetwork.c
+++ lednetwork.c
@@ -232,6 +232,7 @@ int main(void) {
 		 }
 		// loop thru neurons
 		 loop_spikes = 0; 
+		 
 		for (ch = 0; ch < nch; ch++) {
 			printf("\n %f", v[ch])
 			if (v[ch] >= 0) { // if neuron is in integrate mode
