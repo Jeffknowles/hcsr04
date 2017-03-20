e8346c02e8bc9893b0e1b07c3a36049ce66445ef
jknowles <jeff.knowles@gmail.com>
2017-03-20 11:30:32 -0700

    autocommit

--- lednetwork.c
+++ lednetwork.c
@@ -258,7 +258,7 @@ int main(void) {
 			    }
 		    else { // otherwise if neuron is in spike mode
 		      if (v[ch] < (double) -1 * spike_len[ch]) { // if the time since spike onset is up, end spike
-		        printf("\n %f", v[ch])
+		        printf("\n %f", v[ch]);
 		        v[ch] = 0; // set voltage to 0
 		        ledscape_set_color(frame, 0, ch, rgb_off[0], rgb_off[1], rgb_off[2]);
 		        
