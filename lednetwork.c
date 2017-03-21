e7b4561fa6639c8b52d3c6371980f99f4adeef15
jknowles <jeff.knowles@gmail.com>
2017-03-20 22:05:51 -0700

    autocommit

--- lednetwork.c
+++ lednetwork.c
@@ -60,7 +60,7 @@ const double k = 2; // magnitude of the leak
 
 
 // connection settings - declare connections between neurons
-#define maxCon 20
+#define maxCon 10
 #define nch 10 // number of neurons
 #define num_pixels 10
 
