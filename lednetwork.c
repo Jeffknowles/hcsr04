45cea67f01f2ade7807c3d642159c4d229ac6601
jknowles <jeff.knowles@gmail.com>
2017-03-20 20:53:55 -0700

    autocommit

--- lednetwork.c
+++ lednetwork.c
@@ -135,7 +135,7 @@ int main(void) {
 		weights[ii][3] = 9;
 		for (iii=3; iii<maxCon; iii++){
 			connections[ii][iii]=1+rand() % nch-1;
-			weights[ii][iii] = random_float((float) -1, (float) 1);
+			weights[ii][iii] = random_float((float) 0, (float) 0);
 		}
 
 		if (ii > 2){
