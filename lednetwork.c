80a00a5a5fe21b738cb47277e3c8eaaf14c4d89c
jknowles <jeff.knowles@gmail.com>
2017-03-20 20:32:16 -0700

    autocommit

--- lednetwork.c
+++ lednetwork.c
@@ -239,7 +239,7 @@ int main(void) {
 		    	v[ch] = fmax(v[ch], 0);
 		    	v[ch] = fmin(v[ch], thresh+1);
 
-		    	ledscape_set_color(frame, 0, ch, (uint8_t) 0, (uint8_t) 0,(uint8_t) ((double) rgb_spike[ch][2] * v[ch] / (thresh)); 
+		    	ledscape_set_color(frame, 0, ch, (uint8_t) 0, (uint8_t) 0,(uint8_t) ((double) rgb_spike[ch][2] * v[ch] / (thresh))); 
 			    // if the neuron crosses threshold, fire and increment outputs
 			    if (v[ch] > thresh) {
 			        loop_spikes = loop_spikes+1;
