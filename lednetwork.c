2eb99b214c4052309df05a89007a8f0d23d80b38
jknowles <jeff.knowles@gmail.com>
2017-03-20 22:25:56 -0700

    autocommit

--- lednetwork.c
+++ lednetwork.c
@@ -56,7 +56,7 @@ const bool pong_only_in_range = true;
 	
 
 const double thresh = 20;
-const double k = 1; // magnitude of the leak
+const double k = 2; // magnitude of the leak
 
 
 // connection settings - declare connections between neurons
