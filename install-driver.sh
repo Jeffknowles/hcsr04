f3f4309c36c314fd1663e14409b62f33c6f31274
jknowles <jeff.knowles@gmail.com>
2017-03-21 20:40:23 -0700

    autocommit

--- install-driver.sh
+++ install-driver.sh
@@ -4,7 +4,7 @@ export SLOTS=/sys/devices/bone_capemgr.9/slots
 export PINS=/sys/kernel/debug/pinctrl/44e10800.pinmux/pins
 export PINMUX=/sys/kernel/debug/pinctrl/44e10800.pinmux/pinmux-pins
 export PINGROUPS=/sys/kernel/debug/pinctrl/44e10800.pinmux/pingroups
- echo BB-ADC > /sys/devices/bone_capemgr.*/slots  
+echo BB-ADC > /sys/devices/bone_capemgr.*/slots  
 
 echo hcsr04 > $SLOTS
 
