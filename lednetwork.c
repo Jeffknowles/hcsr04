c2bf319082e45e00707e77e6763bfb34f39dc8fc
jknowles <jeff.knowles@gmail.com>
2017-03-23 20:16:31 -0700

    autocommit

--- lednetwork.c
+++ lednetwork.c
@@ -123,7 +123,7 @@ int main(void) {
 	for ( ii=0; ii<(nch-5); ii++){
 
 		connections[ii][0] = ii + 1; 
-		weights[ii][0] = 15;
+		weights[ii][0] = 18;
 		connections[ii][1] = ii + 2; 
 		weights[ii][1] = 10;
 		connections[ii][2] = ii + 3;
