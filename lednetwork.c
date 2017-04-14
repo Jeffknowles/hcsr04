563de794dc8c52d8b60a27927e303ee0fefa75ea
jknowles <jeff.knowles@gmail.com>
2017-04-13 22:28:13 -0700

    autocommit

--- lednetwork.c
+++ lednetwork.c
@@ -169,7 +169,7 @@ int main(void) {
 	for (ii=linear_layer_length; ii<(nch); ii++){ // random synapses onto second layer
 		for (iii=3; iii<maxCon; iii++){
 			connections[ii][iii]=linear_layer_length + (uint32_t) myrandint( (uint32_t) (nch-linear_layer_length));
-			if (random_float((float) 0., (float) 1.) < 0.1) {
+			if (random_float((float) 0., (float) 1.) < 0.5) {
 				weights[ii][iii] = random_float((float) -5, (float) 0.);
 			}
 			else {
