60441c742438e450fe292077f9e4a08444c2ffc9
jknowles <jeff.knowles@gmail.com>
2017-04-13 21:09:15 -0700

    autocommit

--- lednetwork.c
+++ lednetwork.c
@@ -38,8 +38,8 @@ const double ao_max = 4096;
 // connection settings - declare connections between neurons
 // connection settings - declare connections between neurons
 #define maxCon 40
-#define nch 150 // number of neurons
-#define num_pixels 150
+#define nch 250 // number of neurons
+#define num_pixels 250
 
 
 int readao( FILE* f0 ) {
