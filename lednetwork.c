368799af6f95f1ffa7739aa3e56aaeedb27ff93a
jknowles <jeff.knowles@gmail.com>
2017-04-16 23:09:50 -0700

    autocommit

--- lednetwork.c
+++ lednetwork.c
@@ -226,7 +226,7 @@ int main(void) {
     struct timespec tim, tim2;
     tim.tv_sec = 1;
     tim.tv_nsec = 500;
-    nanosleep(&tim , &tim2)
+    nanosleep(&tim , &tim2);
 
 	// flash lights
 	for (i=0; i<10;i++){
