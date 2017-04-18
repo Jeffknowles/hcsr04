9ed9dafdc3c7bb0316393624fbd56d2646cedfb2
jknowles <jeff.knowles@gmail.com>
2017-04-17 22:34:18 -0700

    autocommit

--- lednetwork.c
+++ lednetwork.c
@@ -63,7 +63,7 @@ double doPing(unsigned int *pruData) {
 }
 
 
-void doStartupLightDisplay(ledscape_t *leds, ledscape_frame_t *frame, , unsigned *frame_num, uint8_t *rgb_off, uint8_t *rgb_spike[][3])
+void doStartupLightDisplay(ledscape_t *leds, ledscape_frame_t *frame,  unsigned *frame_num, uint8_t *rgb_off, uint8_t *rgb_spike[][3])
 {
 
 	uint32_t i;
@@ -108,7 +108,7 @@ void doStartupLightDisplay(ledscape_t *leds, ledscape_frame_t *frame, , unsigned
 	  		ledscape_draw(leds, frame_num);
 	  	}
 
-	  	printf("poop %d", num_pixels);
+	  	printf("poop %d", num_pixels );
 	// return (int) 1;
 }
 
