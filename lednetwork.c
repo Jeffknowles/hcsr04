0aefa42ec40342ee6a8c7f8bccce5256a1f1650c
jknowles <jeff.knowles@gmail.com>
2017-07-19 17:58:49 -0700

    autocommit

--- lednetwork.c
+++ lednetwork.c
@@ -373,7 +373,8 @@ int main(void) {
 			 }
 		}
 		// set audio input nodes based on a1 (sensitivity) a2 (sound envelope; see spec) 
-		ao_values = readao(a2);
+		ao_values[0] = readao(a1);
+		ao_values[0] = readao(a1);
 		for (ch = 0; ch < num_sound_inputs; ch++){
 			  // set v[0] based on sonar
 			 if ((double) ao_values / ao_max >= 0.1 & v[sound_inputs[ch]] >= 0) {
