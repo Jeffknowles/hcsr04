83b32288f2bc9e3e885691473857b1123b05d50c
jknowles <jeff.knowles@gmail.com>
2017-07-19 17:35:02 -0700

    autocommit

--- lednetwork.c
+++ lednetwork.c
@@ -375,7 +375,7 @@ int main(void) {
 		ao_values = readao(a0);
 		for (ch = 0; ch < num_sound_inputs; ch++){
 			  // set v[0] based on sonar
-			 if ((double) ao_values / ao_max >= 0.1) {
+			 if ((double) ao_values / ao_max >= 0.1 & v[sound_inputs[ch]] >= 0) {
 			    v[sound_inputs[ch]] = v[sound_inputs[ch]] + (double) ao_values * 5 / ao_max;
 			 }
 		}
