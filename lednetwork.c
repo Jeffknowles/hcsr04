71ba2c5ac035e6cc2cbe7924e036268223319993
jknowles <jeff.knowles@gmail.com>
2017-07-19 18:01:45 -0700

    autocommit

--- lednetwork.c
+++ lednetwork.c
@@ -378,7 +378,7 @@ int main(void) {
 		for (ch = 0; ch < num_sound_inputs; ch++){
 			  // set v[0] based on sonar
 			 if ((double) ao_values / ao_max >= 0.1 & v[sound_inputs[ch]] >= 0) {
-			    v[sound_inputs[ch]] = v[sound_inputs[ch]] + (double) ao_values * 2 / ao_max;
+			    v[sound_inputs[ch]] = v[sound_inputs[ch]] + ((double) 2*ao_values[0] / ao_max)*((double) ao_values[1] / ao_max);
 			 }
 		}
 		// set touch input nodes based on a3 (sensitivity) a4 (touch resistance analog circuit; see spec) 
@@ -387,7 +387,7 @@ int main(void) {
 		for (ch = 0; ch < num_touch_inputs; ch++){
 			  // set v[0] based on sonar
 			 if ((double) ao_values / ao_max >= 0.1 & v[touch_inputs[ch]] >= 0) {
-			    v[touch_inputs[ch]] = v[touch_inputs[ch]] + (double) ao_values * 2 / ao_max;
+			    v[touch_inputs[ch]] = v[touch_inputs[ch]] + (double) ao_values[0] /;
 			 }
 		}
 
