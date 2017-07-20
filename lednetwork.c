63e4a9a7f98d890c4a3be29c100ec97334d7c312
jknowles <jeff.knowles@gmail.com>
2017-07-19 17:00:39 -0700

    autocommit

--- lednetwork.c
+++ lednetwork.c
@@ -220,7 +220,7 @@ int main(void) {
 	// setup input parameters
 	uint32_t sonar_inputs[num_sonar_inputs];
 	sonar_inputs[0] = (uint32_t) 0;
-	sonar_inputs[1] = (uint32_t) 1;
+	sonar_inputs[1] = (uint32_t) 50;
 
 	printf("%f", thresh);
 	// initialize neurons 
