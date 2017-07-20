8fb4e55c47697b461df8b5827e3dd408cb78070b
jknowles <jeff.knowles@gmail.com>
2017-07-19 18:51:22 -0700

    autocommit

--- lednetwork.c
+++ lednetwork.c
@@ -135,7 +135,7 @@ void doStartupLightDisplay(ledscape_t *leds, ledscape_frame_t *frame,  unsigned 
 	ledscape_draw(leds, frame_num);
 	nanosleep(&tim , &tim2);
 	for (ii=0; ii<np; ii++){
-	  		// ledscape_set_co lor(frame, 0, ii, rgb_spike[ii][0], rgb_spike[ii][1], rgb_spike[ii][2]);
+	  		ledscape_set_co lor(frame, 0, ii, rgb_spike[ii][0], rgb_spike[ii][1], rgb_spike[ii][2]);
 	  	    ledscape_draw(leds, frame_num);
 	  		nanosleep(&tim , &tim2);
 	  		// ledscape_set_color(frame, 0, ii, rgb_off[0], rgb_off[1], rgb_off[2]);
