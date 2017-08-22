a21f3d7db9f7ec592d6b8d6123c79b4004697d89
jknowles <jeff.knowles@gmail.com>
2017-08-22 15:44:12 -0700

    autocommit

--- lednetwork.c
+++ lednetwork.c
@@ -600,7 +600,7 @@ int main(void) {
 
 
 		// read and interperate input 
-		ao_values[0] = buffer_AIN_0[0];
+		ao_values[0] = (uint32_t) buffer_AIN_0[0];
 		sense_thresh = (((double) ao_values[0]) / ((double) ao_max))*sense_thresh_i;
 		printf("%f\n", sense_thresh);
 		currentIPI = (double)(01 * sense_thresh * 2 * 29)/1000000; //set ipi based on a0
