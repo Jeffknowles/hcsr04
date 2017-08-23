4dbeff3ebb8fc08d93bd12534c1ca608e95f9b82
jknowles <jeff.knowles@gmail.com>
2017-08-22 18:16:12 -0700

    autocommit

--- install-driver.sh
+++ install-driver.sh
@@ -5,7 +5,7 @@ export PINS=/sys/kernel/debug/pinctrl/44e10800.pinmux/pins
 export PINMUX=/sys/kernel/debug/pinctrl/44e10800.pinmux/pinmux-pins
 export PINGROUPS=/sys/kernel/debug/pinctrl/44e10800.pinmux/pingroups
 
-# echo BBBIO-EHRPWM > $SLOTS
+echo BBBIO-JKPWM > $SLOTS
 echo BB-ADC > $SLOTS
 
 # echo BBIO
